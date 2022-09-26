ec806e4f8485a1585d31a6b38379ad8eb8ac6897
Israel Ludolf <61331235+iLudolf@users.noreply.github.com>
2022-09-25 22:22:24 -0300

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,21 @@
 import { Header } from "../../components/Header"
-import { Container } from "./styles"
+import { Body, Container, InputSearch, ButtonSearch, GroupSearch } from "./styles"
+import {Plus, PlusCircle} from "phosphor-react"
 
 export const Home = () => {
     return (
         <Container>
             <Header />
+            <Body>
+                <GroupSearch>
+                    <InputSearch 
+                        placeholder="Adicione uma nova tarefa"
+                    />
+                    <ButtonSearch >Criar 
+                       {" "} <Plus size={15} color="#ffff"/>
+                    </ButtonSearch>
+                </GroupSearch>
+            </Body>
         </Container>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,3 +4,42 @@ export const Container = styled.nav`
 width: 100%;
 height: 100vh;
 `
+
+export const Body = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+flex-direction: column;
+padding-left: 352px;
+padding-right: 352px;
+`
+
+export const InputSearch = styled.input`
+flex: 4;
+width: 100%;
+padding: 16px;
+border-radius: 8px;
+border: none;
+border: 2px solid transparent;
+&:focus{
+    border: 2px solid ${({ theme}) => theme.blue};
+}
+margin-right: 8px;
+`
+
+export const ButtonSearch = styled.button`
+flex: 1;
+width: 736px;
+height: 54px;
+border-radius: 8px;
+background-color: ${({ theme}) => theme.blue};
+border: none;
+align-items: center;
+`
+
+export const GroupSearch = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+transform: translatey(-50%);
+`
